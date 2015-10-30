ad5ca27884cc2240f314441f62014732a09cba1e
johnwonder <johnufida@163.com>
2015-10-30 17:17:53 +0800

    express框架学习 MEAN开发

---
+++ public/javascripts/vidzy.js
@@ -0,0 +1,16 @@
+var app = angular.module('Vidzy',['ngRoute']);
+
+app.config(['$routeProvider'],function($routeProvider){
+
+	$routeProvider
+		.when('/',{
+
+			templateUrl:'partials/home.html'
+		})
+		.otherwise({
+
+			redirectTo:'/'
+		})
+
+
+})
