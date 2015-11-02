c5a2994e92f45c5a87714707bb083268ad084465
johnwonder <johnufida@163.com>
2015-11-02 23:02:21 +0800

    add controller and restful api get videos

--- public/javascripts/vidzy.js
+++ public/javascripts/vidzy.js
@@ -1,11 +1,21 @@
-var app = angular.module('Vidzy',['ngRoute']);
+var app = angular.module('Vidzy',['ngResource','ngRoute']);
+
+app.controller('HomeCtrl',['$scope','$resource',function($scope,$resource){
+
+		var Videos = $resource('/api/videos');
+		Videos.query(function(videos){
+			$scope.videos = videos;
+		})
+
+}])
 
 app.config(['$routeProvider',function($routeProvider){
 
 	$routeProvider
 		.when('/',{
 
-			templateUrl:'partials/home.html'
+			templateUrl:'partials/home.html',
+			controller:'HomeCtrl'
 		})
 		.otherwise({
 
